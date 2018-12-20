ccec573b72def9f6aa147863bc79f796a5e82236
kunal <kunal@agnitioworld.com>
2018-12-20 17:57:37 +0530

    20-dec-2018(office)

--- app/src/main/java/com/sportsgen/Organiser/Fragments/EventsFragment.java
+++ app/src/main/java/com/sportsgen/Organiser/Fragments/EventsFragment.java
@@ -1,17 +1,21 @@
 package com.sportsgen.Organiser.Fragments;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.content.res.AppCompatResources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.leinardi.android.speeddial.FabWithLabelView;
 import com.leinardi.android.speeddial.SpeedDialActionItem;
 import com.leinardi.android.speeddial.SpeedDialView;
 import com.sportsgen.R;
-import com.sportsgen.User.Fragments.TournamentFragment;
 
 public class EventsFragment extends Fragment {
     public static Fragment newInstance(){
@@ -23,16 +27,21 @@ public class EventsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_organiser_events,container,false);
         SpeedDialView speedDialView=v.findViewById(R.id.fab_speed_dial);
-        speedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.fab_long_label, R.drawable.ic_add_white_24dp)
-                        .setLabel("Label 1")
-                        .create()
-        );
-        speedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.fab_long_label, R.drawable.ic_add_white_24dp)
-                        .setLabel("Label 2")
-                        .create()
-        );
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_long_label, R.drawable
+                .ic_add_white_24dp)
+                .setLabel("label 1")
+                .create());
+
+        Drawable drawable2 = AppCompatResources.getDrawable(getActivity(), R.drawable.ic_add_white_24dp);
+        FabWithLabelView fabWithLabelView2 = speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id
+                .fab_custom_color, drawable2)
+                .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.colorwhite,null))
+                .setLabel("label 2")
+                .setLabelColor(Color.BLACK)
+                .setLabelBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorwhite,null))
+                .create());
+
         return v;
     }
 }
